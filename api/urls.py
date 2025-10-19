96eaafd1cc8cfc689b6e2aac85dc3cc5d6b9cd04
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-10-19 16:54:53 +0600

    Teacher course views implement and view permission implement

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path,include
 from rest_framework_nested import routers
 from department.views import DepartmentViewSet
-from course.views import CourseViewSet
+from course.views import CourseViewSet,TeacherCourseViewSet
 from enrollment.views import EnrollmentViewSet
 
 router = routers.DefaultRouter()
 router.register('departments',DepartmentViewSet,basename='departments')
 router.register('courses',CourseViewSet,basename='courses')
 router.register('enrollments',EnrollmentViewSet,basename='enrollments')
+router.register('teacher/courses',TeacherCourseViewSet,basename='teacher_courses')
 
 urlpatterns = [
    path('',include(router.urls)),
--- course/views.py
+++ course/views.py
@@ -3,12 +3,22 @@ from .models import Course
 from .serializers import CourseSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from permission.permission import IsTeacher
 
 # Create your views here.
 
 class CourseViewSet(ModelViewSet):
+    http_method_names =['get']
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     filter_backends = [DjangoFilterBackend,SearchFilter]
     filterset_fields = ['department']
     search_fields = ['title']
+
+class TeacherCourseViewSet(ModelViewSet):
+    # http_method_names =['get','put','patch','delete']
+    # queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsTeacher]
+    def get_queryset(self):
+        return Course.objects.filter(teacher=self.request.user)
