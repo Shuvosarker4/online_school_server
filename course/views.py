fe03c5d247e578a4c0e2633282ca169a9d817a69
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-10-19 14:31:51 +0600

    Course filter by name and department implement

--- course/views.py
+++ course/views.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Course
 from .serializers import CourseSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 # Create your views here.
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filterset_fields = ['department']
+    search_fields = ['title']
